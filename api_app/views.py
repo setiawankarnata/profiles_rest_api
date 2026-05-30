b15d234ac6bf10e4d58ca0515ec010f57dcb6662
Setiawan Karnata <setiawankarnata@gmail.com>
2026-05-30 19:06:37 +0700

    Add permissions.py and related files

--- api_app/views.py
+++ api_app/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
 from rest_framework import viewsets
+from .models import UserProfile
+from . import permissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class HelloApiView(APIView):
@@ -86,3 +89,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({"http_method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
